6a9ba7c554ecff6d93a141bf9ae191a9b3467cf3
nobreozeas <nobre.ozeas@gmail.com>
2022-06-09 22:09:44 -0500

    fi

--- tests/Feature/UsuarioTest.php
+++ tests/Feature/UsuarioTest.php
@@ -3,19 +3,50 @@
 
 use App\Models\Usuario;
 
-it('nome do usuario deve ser maior ou igual que 3 letras e possuir apenas caracteres', function(){
-
-   
+it('cpf deve ser valido', function(){
 
     $usuario = new Usuario();
 
-    $nome = $usuario->nome = '';
+    $doc = $usuario->cpf = '03357858240';
 
-    
+    $cpf = preg_replace('/[^0-9]/', "", $doc);
+
+    if (strlen($cpf) !== 11 || preg_match('/(\d)\1{10}/', $cpf)) {
+        $this->assertTrue(false);
+    }
+
+    for ($t = 9; $t < 11; $t++) {
+        for ($d = 0, $c = 0; $c < $t; $c++) {
+            $d += $cpf[$c] * (($t + 1) - $c);
+        }
+
+        $d = ((10 * $d) % 11) % 10;
+
+        if ($cpf[$c] != $d) {
+            $this->assertTrue(false);
+
+        }
+    }
+    $this->assertTrue(true);
 
 
-    
-    expect(true)->toBeTrue();
+
+
+   
+});
+
+it('nome precisa ter mais que 2 caracteres e nao pode conter numeros', function(){
+
+    $usuario = new Usuario();
+
+    $nome = $usuario->nome = 'ozeas';
+
+    if(strlen($nome) >= 2){
+        return preg_match('/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/',$nome) ? $this->assertTrue(true) : $this->assertTrue(false);
+        
+    }else{
+        $this->assertTrue(false);
+    }
 });
 
 // function validaSobrenome($sobrenome){
